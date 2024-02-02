60eb6bd8d476dd29bacf89d9f8d4d9db573ea14c
Ali Sadeghi <ali909392@gmail.com>
2024-02-02 22:16:47 +0330

    Add knockout.js files

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-console.log("Hi");
+function HelloWorldViewModel() {
+    this.message = ko.observable("Hello");
+    this.logHello = function () {
+        console.log(this.message());
+    };
+}
+
+var viewModel = new HelloWorldViewModel();
+ko.applyBindings(viewModel);
