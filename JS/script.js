957274b1411aa8dce048a6ed7bcc460d55fe750f
Ali Sadeghi <ali909392@gmail.com>
2024-05-02 20:14:35 +0330

    1403-02-13

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
 console.log("Hi");
+
+// document.querySelector("#hi").innerHTML = "hello";
+
+/* document.write("hi"); */
+
+// const hello = "😎✨";
+
+// var hi = "hello";
+
+// let hi = "hello";
+
+// const hi = `hel${hi}lo`;
+
+console.log(Math.PI);
