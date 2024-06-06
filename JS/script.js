fef17e43d84898e8784f131bce7939a3bf6a51f0
Ali Sadeghi <ali909392@gmail.com>
2024-06-06 16:11:43 +0330

    1403-03-10

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,21 @@
 console.log("Hi");
+
+const a = 10;
+
+switch (a) {
+    case 1:
+        console.log("1");
+        break;
+    case 10:
+        console.log("10");
+        break;
+    case 20:
+        console.log("20");
+    case 30:
+        console.log("30");
+        break;
+    default:
+        console.log("hi");
+}
+
+10 < 20 ? console.log("true") : console.log("false");
