6996c9131a5ea44ff79927bff2497e64792d44ce
Ali Sadeghi <ali909392@gmail.com>
2024-06-06 19:01:02 +0330

    1402-03-17

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,64 @@
-console.log("Hi");
+"use strict";
 
-const a = 10;
+// در سطح گلوبال
+console.log(this); // undefined
 
-switch (a) {
-    case 1:
-        console.log("1");
-        break;
-    case 10:
-        console.log("10");
-        break;
-    case 20:
-        console.log("20");
-    case 30:
-        console.log("30");
-        break;
-    default:
-        console.log("hi");
+function test1() {
+    console.log(this); // undefined
 }
 
-10 < 20 ? console.log("true") : console.log("false");
+const test2 = () => {
+    console.log(this);
+};
+
+test1();
+test2();
+
+let user = {
+    name: "zahra",
+    age: 24,
+};
+
+function showUser(city) {
+    // console.log(this.age, city);
+
+    this.city = city;
+}
+
+showUser.call(user);
+
+showUser.apply(user, ["tabriz"]);
+
+// console.log(user);
+
+let user1 = {
+    name: "zahra",
+    age: 24,
+};
+
+function showUser() {
+    // console.log(this); //24 undefined
+}
+
+showUser.bind(user1);
+
+const person = {
+    name: "Ali",
+    age: 21,
+    greet: function (greeting) {
+        console.log(
+            `${greeting}, my name is ${this.name} and I am ${this.age} years old.`
+        );
+    },
+};
+
+function welcome(greeting1, greeting2) {
+    console.log(`${greeting1} and ${greeting2}, I am ${this.name}.`);
+}
+
+const newGreetFunction = person.greet.bind(person, "Hello");
+newGreetFunction(); // Hello, my name is Ali and I am 21 years old.
+
+welcome.call(person, "Hi", "Aloha"); // Hi and Aloha, I am Ali.
+
+welcome.apply(person, ["Hi", "Aloha"]); // Hi and Aloha, I am Ali.
