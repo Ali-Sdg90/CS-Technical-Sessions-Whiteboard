dd71176aedf08ec161a1108a3b9c6bcba7687adf
Ali Sadeghi <ali909392@gmail.com>
2024-06-20 16:16:58 +0330

    1403-03-24

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
 console.log("Hi");
+
+const age = 90;
+const name = "name";
+
+console.log("hi" + name);
+
+console.log("hi my name \n is ALI and \n\n\n\n im 90 yo");
+console.log(`hi my name
+
+                is ${name} and im
+
+            ${age} yo`);
+// console.log("hi my name is " + name + " and im " + age + " yo");
